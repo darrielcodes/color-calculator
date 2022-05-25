eecc5b4f217616364fe8ac2f6f0ef833234d102a
darrielcodes <darriel.morris@codeimmersives.comgit config --global user.email darrielmorris@yahoo.com>
2022-05-25 11:28:57 -0700

    Completed color calculator project

---
+++ main.js
@@ -0,0 +1,37 @@
+const prompt = require('prompt-sync')({sigint: true});
+
+let userInput = prompt("Do you want to combine or deconstruct a color?: ")
+let x = "combine"
+let y = "deconstruct"
+
+
+if (userInput === x){
+    let color1 = prompt("Enter your first color: ")
+    let color2 = prompt("Enter your second color: ")
+
+    if ((color1 === "red") && (color2 === "blue")){
+    console.log("purple")
+}
+    else if ((color1 === "red") && (color2 === "yellow")){
+    console.log("orange")
+}
+    else if ((color1 === "blue") && (color2 === "yellow")){
+    console.log("green") 
+    }
+    else (console.log("error"))
+}
+
+if (userInput === y){
+    let color3 = prompt("Enter a color to deconstruct: ")
+
+    if (color3 === "purple"){
+        console.log("red + blue")
+    }
+    else if(color3 === "orange"){
+        console.log("red + yellow")
+    }
+    else if(color3 === "green"){
+        console.log("blue + yellow")
+    }
+    else (console.log("error"))
+}
